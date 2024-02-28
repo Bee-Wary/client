62a25a2f0641720ac705b0ea04b17c6df3cec281
Zion Van der Wee <r0784079@student.thomasmore.be>
2024-02-28 14:36:39 +0100

    Mantine implemented in client

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,20 @@
-import "@/styles/globals.scss";
-import type { AppProps } from "next/app";
+import '@/styles/globals.scss';
+import '@mantine/core/styles.css';
+import type { AppProps } from 'next/app';
+import { createTheme, MantineProvider } from '@mantine/core';
+
+const theme = createTheme({
+  fontFamily: 'Archivo Black',
+  colors: {
+    DustyOrange: ['#F18624', '#F18624', '#F18624', '#F18624', '#F18624', '#F18624', '#F18624', '#F18624', '#F18624', '#F18624'],
+  },
+  primaryColor: 'DustyOrange',
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <MantineProvider theme={theme} defaultColorScheme="dark">
+      <Component {...pageProps} />
+    </MantineProvider>
+  );
 }
