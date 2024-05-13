c4419d49b948951ce8e130c4bf829c2863b432d8
Bo Robbrecht <robbrechtbo@hotmail.com>
2024-05-13 21:19:54 +0200

    Extracted HiveCard to it's own component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,8 @@ import styles from '@/styles/page.module.scss';
 import Image from 'next/image';
 import Link from 'next/link';
 import { MapPin, CalendarBlank, Heartbeat, Plugs } from "@phosphor-icons/react/dist/ssr";
+import HiveCard from '@/components/HiveCard';
+
 export default async function Beehivespage() {
   const data = (await getSummerizedBeehives()).documents;
 
@@ -14,18 +16,15 @@ export default async function Beehivespage() {
           <Link href="#">View all</Link>
         </div>
         {data.map(doc =>
-          <article key={doc._id} className={styles.hive}>
-            <Image src="https://placehold.co/400x400/png" width={400} height={400} alt={`Image of ${doc.name}`} />
-            <div className={styles.hiveInfo}>
-              <div className={styles.hiveHeader}>
-                <h3>{doc.name}</h3>
-                {doc.last_inspection && <b className={(doc.last_inspection.illness ? styles.badIndicator : styles.positiveIndicator)}><Heartbeat weight='fill' /></b>}
-                {doc.last_sensor_entry && <b className={styles.positiveIndicator}><Plugs weight='fill' /></b>}
-              </div>
-              {doc.last_inspection && <p><CalendarBlank weight='fill' /> <span className={styles.label}>Last checkup:</span> {new Date(doc.last_inspection.last_updated).toLocaleDateString()} | {new Date(doc.last_inspection.last_updated).toLocaleTimeString()}</p>}
-              <p><MapPin weight='fill' /><span className={styles.label}>Location:</span> {doc.location.coordinates.map(c => <span key={c}>{c} </span>)}</p>
-            </div>
-          </article>
+          <HiveCard
+            key={doc._id}
+            img="https://placehold.co/400x400/png"
+            name={doc.name}
+            lastInspection={doc.last_inspection ? new Date(doc.last_inspection.last_updated) : undefined}
+            illness={(doc.last_inspection && doc.last_inspection.illness) ? true : false}
+            location={doc.location.coordinates}
+            sensor={doc.last_sensor_entry ? true : false}
+            />
         )}
       </section>
     </main>
