649116a2c61fe6ba444c7d0724757935193aa15f
Bo Robbrecht <robbrechtbo@hotmail.com>
2024-05-13 21:09:53 +0200

    Added styling for HiveCard

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,7 @@ import { getSummerizedBeehives } from '@/services/beehives/queries';
 import styles from '@/styles/page.module.scss';
 import Image from 'next/image';
 import Link from 'next/link';
-import { MapPin, CalendarBlank } from "@phosphor-icons/react/dist/ssr";
-
+import { MapPin, CalendarBlank, Heartbeat, Plugs } from "@phosphor-icons/react/dist/ssr";
 export default async function Beehivespage() {
   const data = (await getSummerizedBeehives()).documents;
 
@@ -15,11 +14,17 @@ export default async function Beehivespage() {
           <Link href="#">View all</Link>
         </div>
         {data.map(doc =>
-          <article key={doc._id}>
-            <Image src="https://placehold.co/400x400/png" width={400} height={400} alt={`Image of ${doc.name}`}/>
-            <h3>{doc.name}</h3>
-            {doc.last_inspection && <p><CalendarBlank weight='fill'/>{new Date(doc.last_inspection.last_updated).toLocaleDateString()} {new Date(doc.last_inspection.last_updated).toLocaleTimeString()}</p>}
-            <p><MapPin weight='fill'/>Location: {doc.location.coordinates.map(c => <span key={c}>{c} </span>)}</p>
+          <article key={doc._id} className={styles.hive}>
+            <Image src="https://placehold.co/400x400/png" width={400} height={400} alt={`Image of ${doc.name}`} />
+            <div className={styles.hiveInfo}>
+              <div className={styles.hiveHeader}>
+                <h3>{doc.name}</h3>
+                {doc.last_inspection && <b className={(doc.last_inspection.illness ? styles.badIndicator : styles.positiveIndicator)}><Heartbeat weight='fill' /></b>}
+                {doc.last_sensor_entry && <b className={styles.positiveIndicator}><Plugs weight='fill' /></b>}
+              </div>
+              {doc.last_inspection && <p><CalendarBlank weight='fill' /> <span className={styles.label}>Last checkup:</span> {new Date(doc.last_inspection.last_updated).toLocaleDateString()} | {new Date(doc.last_inspection.last_updated).toLocaleTimeString()}</p>}
+              <p><MapPin weight='fill' /><span className={styles.label}>Location:</span> {doc.location.coordinates.map(c => <span key={c}>{c} </span>)}</p>
+            </div>
           </article>
         )}
       </section>
