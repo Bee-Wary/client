d2a62c14fd0f49cdb2dbec60bd103ed50e87284e
Bo Robbrecht <robbrechtbo@hotmail.com>
2024-05-15 12:48:09 +0200

    Displaying notes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,22 @@
 import { getSummerizedBeehives } from '@/services/beehives/queries';
 import styles from '@/styles/page.module.scss';
-import Image from 'next/image';
 import Link from 'next/link';
-import { MapPin, CalendarBlank, Heartbeat, Plugs } from "@phosphor-icons/react/dist/ssr";
 import HiveCard from '@/components/HiveCard';
+import { getSummerizedInspections } from '@/services/inspections/queries';
+import { CaretRight } from '@phosphor-icons/react/dist/ssr';
 
 export default async function Beehivespage() {
-  const data = (await getSummerizedBeehives()).documents;
+  const beehives = (await getSummerizedBeehives()).documents;
+  const inspections = (await getSummerizedInspections()).documents;
 
   return (
     <main className={styles.main}>
-      <section className={styles.beehives}>
+      <section className={styles.itemList}>
         <div className={styles.sectionTitle}>
           <h2>Beehives</h2>
           <Link href="#">View all</Link>
         </div>
-        {data.map(doc =>
+        {beehives.map(doc =>
           <HiveCard
             key={doc._id}
             img="https://placehold.co/400x400/png"
@@ -26,6 +27,27 @@ export default async function Beehivespage() {
             sensor={doc.last_sensor_entry ? true : false}
             />
         )}
+      </section>
+      <section className={styles.itemList}>
+        <div className={styles.sectionTitle}>
+          <h2>Recent Notes</h2>
+          <Link href="#">View all</Link>
+        </div>
+        <div className={styles.noteList}>
+          <div>
+            <span>Name</span>
+            <span>Datum</span>
+            <span>Status</span>
+            <CaretRight weight='regular' />
+          </div>
+          {inspections.map(doc => 
+            <a href={`inspections/${doc._id}`} key={doc._id}>
+              <span>{doc.title}</span>
+              <span>{new Date(doc.last_updated).toLocaleDateString()}</span>
+              <span>{doc.draft ? "Draft" : "Finished"}</span>
+              <CaretRight weight='regular' />
+            </a>)}
+        </div>
       </section>
     </main>
   );
