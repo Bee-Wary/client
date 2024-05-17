7696c012eae7376beded863c2bcd6a9784d0788c
TimGrevendonk <r0831309@student.thomasmore.be>
2024-05-17 16:02:51 +0200

    changed bottom navigation to support inspection routing

--- src/components/navigation/BottomNavigationBar.tsx
+++ src/components/navigation/BottomNavigationBar.tsx
@@ -3,15 +3,15 @@ import NavigationbarButton from './NavigationbarButton';
 
 // NextJs server side rendered omponents cannot inherit styles from an ancestor (like IconContext) and must import from .../dist/ssr.
 // https://github.com/phosphor-icons/react/blob/master/README.md#react-server-components-and-ssr 
-import { Archive, FilePlus, ChartBar } from "@phosphor-icons/react/dist/ssr";
+import { Archive, FilePlus, Files  } from "@phosphor-icons/react/dist/ssr";
 
  const BottomNavigationBar = () => {
   return (
     <>
       <div className={styles.navigationBar}>
           <NavigationbarButton icon={<Archive weight="fill"/>} >Hives</NavigationbarButton>
-          <NavigationbarButton icon={<FilePlus weight="fill"/>} route="/inspections">Notes</NavigationbarButton>
-          <NavigationbarButton icon={<ChartBar weight="fill"/>} route="/statistics">Stats</NavigationbarButton>
+          <NavigationbarButton icon={<FilePlus weight="fill"/>} route="/statistics">Add note</NavigationbarButton>
+          <NavigationbarButton icon={<Files  weight="fill"/>} route="/inspections">Notes</NavigationbarButton>
       </div>
     </>
   );
