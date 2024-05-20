596856c32075a7495d515cbec682d6181792e2a2
TimGrevendonk <r0831309@student.thomasmore.be>
2024-05-20 14:49:28 +0200

    added beehivename query

--- src/services/beehives/queries.ts
+++ src/services/beehives/queries.ts
@@ -1,6 +1,29 @@
 import { generateDataApiUrl, generateDataSource, generateRequestHeaders } from "@/utils/dataApi";
 
 /**
+ * Return a list of all beehive names and their ID.
+ * @returns All beehives their names and IDs.
+ */
+export async function getAllBeehiveNamesAndIDs(): Promise<{ documents: BeehiveName[] }> {
+  try {
+    const response = await fetch(generateDataApiUrl("find"), {
+      method: "POST",
+      headers: generateRequestHeaders(),
+      body: JSON.stringify({
+        ...generateDataSource("beehives"),
+        "projection": {
+          "_id": 1, 
+          "name": 1
+        }
+      })
+    })
+    return response.json();
+  } catch (error) {
+    throw new Error(`Realm Data API returned an error at getAllBeehiveNamesAndIDs: ${error}`)
+  }
+}
+
+/**
  * Returns a summerized overview of the beehives, preformatted for the home page
  * @returns Summerized overview of hives
  */
