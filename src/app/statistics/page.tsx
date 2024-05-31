ba8f4ae6b830754d503041c4225acd61e573ccd2
Zion Van der Wee <r0784079@student.thomasmore.be>
2024-05-31 17:17:50 +0200

    graphs changeable when clicking on the filter buttons

--- src/app/statistics/page.tsx
+++ src/app/statistics/page.tsx
@@ -1,13 +1,13 @@
 import { Select, SelectItem } from '@nextui-org/react';
 import { getAllBeehiveNamesAndIDs } from '@/services/server/beehives/queries';
 import { getAllStatistics } from '@/services/server/statistics/queries';
-import { StatiscticsForm } from '@/components/statistics/statisticsForm';
+import { StatisticsForm } from '@/components/statistics/statisticsForm';
 
 const Statisticspage = async ({ params, searchParams }: { params: { inspectionID?: string }; searchParams: { beehiveRefID?: string } }) => {
   const allBeehivesNames: BeehiveName[] = (await getAllBeehiveNamesAndIDs()).documents;
   const allStatistics: StatisticsList[] = (await getAllStatistics()).documents;
 
-  return <StatiscticsForm beehiveNames={allBeehivesNames} StatisticsList={allStatistics} />;
+  return <StatisticsForm beehiveNames={allBeehivesNames} StatisticsList={allStatistics} />;
 };
 
 export default Statisticspage;
