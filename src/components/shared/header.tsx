b210ce72a405caed88a02643266e4fdee380dc39
Bo Robbrecht <robbrechtbo@hotmail.com>
2024-06-04 14:29:59 +0200

    Added NavMenuItems

--- src/components/shared/header.tsx
+++ src/components/shared/header.tsx
@@ -1,15 +1,17 @@
 "use client";
-import { ArrowUUpLeft, BellRinging} from '@phosphor-icons/react/dist/ssr';
+import { ArrowUUpLeft, Bell, BellRinging, Broadcast, CaretRight, ChartBar, Cube, Gear, PencilSimpleLine, UserCircle} from '@phosphor-icons/react/dist/ssr';
 import { Navbar, NavbarContent, NavbarBrand, NavbarItem, NavbarMenuToggle, NavbarMenu } from '@nextui-org/react';
 import Image from 'next/image';
 import { useRouter } from 'next/navigation';
 import Link from 'next/link';
+import NavMenuItem from '../sidebar/NavMenuItem';
 
 export default function Header() {
   const router = useRouter();
   return (
     <Navbar>
       <NavbarContent>
+      {/* TODO: Make sure back button is hidden on index */}
       <ArrowUUpLeft weight='fill' size={32} onClick={() => router.back()} />
         <NavbarBrand>
           <Image src="/mascotte.png" alt="BeeWary" width={50} height={50} />
@@ -23,8 +25,25 @@ export default function Header() {
         {/* TODO: Find a way to replace the default icon*/}
         <NavbarMenuToggle className="sm:hidden" />
       </NavbarContent>
-      <NavbarMenu>
-
+      <NavbarMenu className='bg-tiki-torch-grey text-petal-white-bright'>
+        <div>
+          <h2>BeeWary</h2>
+          <h3>John Doe</h3>
+          <p>Premium Account</p>
+        </div>
+        <div>
+          <h3>Personal</h3>
+          <NavMenuItem text="Account Info" icon={<UserCircle size={32} weight='fill'/>} href="#" />
+          <NavMenuItem text="Settings" icon={<Gear size={32} weight='fill'/>} href="#" />
+          <NavMenuItem text="Notification" icon={<Bell size={32} weight='fill'/>} href="/notifications" />
+        </div>
+        <div>
+          <h3>Technical</h3>
+          <NavMenuItem text="My beehives" icon={<Cube size={32} weight="fill" />} href="#" />
+          <NavMenuItem text="My sensors" icon={<Broadcast size={32} weight="fill" />} href="/Sensors" />
+          <NavMenuItem text="My inspections" icon={<PencilSimpleLine size={32} weight="fill" />} href="/inspections" />
+          <NavMenuItem text="Statistics" icon={<ChartBar size={32} weight="fill" />} href="/statistics" />
+        </div>
       </NavbarMenu>
     </Navbar>
   );
---
+++ src/components/sidebar/NavMenuItem.tsx
@@ -0,0 +1,18 @@
+import { CaretRight, UserCircle } from "@phosphor-icons/react/dist/ssr"
+import Link from "next/link"
+import { UrlObject } from "url";
+
+interface Props {
+    text: string;
+    icon: JSX.Element;
+    href: string | UrlObject;
+}
+
+export default function NavMenuItem({ text, icon, href }: Props) {
+    return (
+    <Link href={href} className="flex flex-row items-center gap-1">
+        {icon}
+        <p className="grow">{text}</p>
+        <CaretRight size={32} weight="fill" />
+    </Link>)
+}
