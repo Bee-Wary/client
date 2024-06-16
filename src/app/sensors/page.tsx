c0f597535c23545044915e2139d85c24aee1f0ba
Zion Van der Wee <r0784079@student.thomasmore.be>
2024-06-16 15:23:43 +0200

    Sensor page gemaakt maar gebruikt niet alle sensor data

--- src/app/sensors/page.tsx
+++ src/app/sensors/page.tsx
@@ -1,11 +1,12 @@
-// Page function:
-const SensorsPage = () => {
+import { getAllBeehiveNamesAndIDs } from '@/services/server/beehives/queries';
+import { SensorsPageForm } from '@/components/sensor/SensorPageForm';
+import { getAllStatistics } from '@/services/server/statistics/queries';
 
-    return (
-        <>
-        <p>SensorsPage</p>
-        </>
-    );
-}
+const SensorsPage = async ({ params, searchParams }: { params: { inspectionID?: string }; searchParams: { beehiveRefID?: string } }) => {
+  const allBeehivesNames: BeehiveName[] = (await getAllBeehiveNamesAndIDs()).documents;
+  const allStatistics: StatisticsList[] = (await getAllStatistics()).documents;
+
+  return <SensorsPageForm beehiveNames={allBeehivesNames} StatisticsList={allStatistics} />;
+};
 
 export default SensorsPage;
